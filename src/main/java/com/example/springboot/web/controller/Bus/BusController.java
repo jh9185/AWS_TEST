7e597881b0580f6de96c7c311d52b97afdc89a1b
jh9185 <89440013+jh9185@users.noreply.github.com>
2022-01-28 23:58:20 +0900

    xlxs 파일 업로드 테스트_2

--- src/main/java/com/example/springboot/web/controller/Bus/BusController.java
+++ src/main/java/com/example/springboot/web/controller/Bus/BusController.java
@@ -3,12 +3,8 @@ package com.example.springboot.web.controller.Bus;
 import com.example.springboot.config.auth.LoginUser;
 import com.example.springboot.config.auth.dto.SessionUser;
 import com.example.springboot.service.bus.BusService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,9 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
