d6df59b3140307c31c0e97b8d4d37c827972cd85
jh9185 <89440013+jh9185@users.noreply.github.com>
2022-02-11 16:14:03 +0900

    220211 TEST 수정

--- src/main/java/com/example/springboot/Component/ApiComponent.java
+++ src/main/java/com/example/springboot/Component/ApiComponent.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
 @Component
 public class ApiComponent {
     @Value("${naver-map-key}")
